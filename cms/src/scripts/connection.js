623382c4a18387b3d15e09e143575ecdb91377cc
AzureAD\GeorgeMarian <george.marian@principal33.com>
2021-09-20 18:56:31 +0300

    add new employee

--- cms/src/scripts/connection.js
+++ cms/src/scripts/connection.js
@@ -1,18 +1,46 @@
+var axios = require("axios");
 const BACKEND = "https://localhost:5001/Employee/";
+
+const instance = axios.create({
+  withCredentials: false,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    crossorigin: "true",
+  },
+});
 
 export async function getAllEmployees() {
   const url = BACKEND + "GetEmployees/";
 
-  var axios = require("axios");
+  return instance.get(url);
+}
 
-  const instance = axios.create({
-    withCredentials: false,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-      crossorigin: "true",
-    },
+export async function addEmployee(employee) {
+  const url = BACKEND + "AddEmployee";
+
+  return instance({
+    method: "post",
+    url: url,
+    data: employee,
+    headers: { 'Content-Type': 'application/json' }
   });
+}
+
+export async function removeEmployeeFromDb(employeeId) {
+  const url = BACKEND + "RemoveEmployee" + `?id=${employeeId}`;
+
+  return instance.delete(url);
+}
+
+export async function sortByName() {
+  const url = BACKEND + "SortEmployees";
+
+  return instance.get(url);
+}
+
+export async function searchByName(employeeName) {
+  const url = BACKEND + "SearchEmployee" + `?name=${employeeName}`;
 
   return instance.get(url);
 }
